36294482448b9836ca27f497d07e4cc57a727978
joaolop3s <joaofcp98@hotmail.com>
2021-06-12 00:29:12 +0100

    adding and listing emails

--- src/app.js
+++ src/app.js
@@ -2,14 +2,17 @@ const { response, request } = require('express')
 const express = require('express')
 const fetch = require('node-fetch')
 const mongodb = require('mongodb')
+const cors = require('cors')
 const MongoClient = mongodb.MongoClient
 
 
 
-const connectionURL = "mongodb://localhost:27017/mydb"
+const connectionURL = "mongodb://localhost:27017"
 const databaseName = "task-manager"
+const databaseEmpresa = "website-empresa"
 const app = express()
-app.use(express.json()); //middleware 
+app.use(express.json(),cors()); //middleware
+
 
 
 const port = 3001
@@ -23,18 +26,31 @@ var aux = ""
 app.get('/', (req, res) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Credentials', true)
-  res.send(list[0])
+  res.send("Welcome")
 })
 
 
 
-app.get('/all_items', (req, res) => {
+app.get('/list', async (req, res) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Credentials', true)
-    res.send({
-        string:"ola",
-        numero : 2
-    })
+
+    
+    let emails = await getEmails()
+    //console.log("->" + emails)
+    res.send(emails)
+})
+
+//post method to add data to array
+//tested by postman only
+app.post('/add', (req,res) => {
+
+  console.log(req.body)
+  let user = req.body.user
+  let message = req.body.message
+
+  insertEmail(user,message)
+  res.send("Success")
 })
 
 
@@ -52,48 +68,69 @@ app.get('/cats' ,  async (req,res) => {
 })
 
 
-//post method to add data to array
-//tested by postman only
-app.post('/add', (req,res) => {
 
-    console.log(req.body)
-
-    aux = String(req.body)
-
-    res.send(aux)
-})
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-MongoClient.connect(connectionURL,{useNewUrlParser:true,useUnifiedTopology: true},(error,client) => {
-  if(error){
-      return console.log("Unable to connect to db")
-  }
-
-  const db = client.db(databaseName)
-
+function findOneUser(){
+  MongoClient.connect(connectionURL,{useNewUrlParser:true,useUnifiedTopology: true},(error,client) => {
+    if(error){
+        return console.log("Unable to connect to db")
+    }
   
-  db.collection('users').insertOne({
+    const db = client.db(databaseName)
+  
+  
+    db.collection('users').findOne({
       name: "tiago",
       age:13
+    }, (err,result) => {
+      console.log(result)
+    })
+  
   })
+}
 
-  db.collection('users').findOne({
-    name: "tiago",
-    age:13
-  }, (err,result) => {
-    console.log(result)
+
+function insertEmail(username,message){
+  MongoClient.connect(connectionURL,{useNewUrlParser:true,useUnifiedTopology: true},(error,client) => {
+    if(error){
+        return console.log("Unable to connect to db")
+    }
+  
+    const db = client.db(databaseEmpresa)
+
+    db.collection('emails').insertOne({
+      content : message,
+      user : {
+        name : username,
+        email : username + "@.com"
+      }
+    })
+
+    console.log("Added to database mail : " + message + "of" + username )
+  
   })
-
-
-})
-
+}
 
 
 
-      
+  async function getEmails(){
+  let client = await MongoClient.connect(connectionURL,
+    {useNewUrlParser:true,useUnifiedTopology: true});
+
+  let db = client.db(databaseEmpresa);
+  try {
+      return db.collection('emails').find({}).toArray()
+  }
+  catch(e){
+    console.log(e)
+  }
+}
+
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
